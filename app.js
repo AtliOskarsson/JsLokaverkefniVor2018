2521c9530047faedc895460ad94962f71734dbd7
AtliOskarsson <atlio@outlook.com>
2018-05-09 14:02:34 +0000

    Leitingar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let concerts = [];
 let concertio = {};
 $.ajax({
-  'url': 'https://apis.is/concerts',
+  'url': 'http://apis.is/concerts',
   'type': 'GET',
   'dataType': 'json',
   'success': function(response) {
@@ -12,14 +12,30 @@ $.ajax({
     console.log(concerts)
     for (let i = 0; i < concerts.length; i++) {
       let source = concerts[i]
-      document.getElementById("hold").innerHTML += "<div> <h2>" + source["eventDateName"] + "</h2> <img src=" + 
+      document.getElementById("hold").innerHTML += "<div id='holder'> <h2>" + source["eventDateName"] + "</h2> <img src=" + 
                                                    source["imageSource"]+"> <p>" + source["eventHallName"] + "<br>" +
                                                    source["dateOfShow"] +  "</div>"
     };
   }
 });
 
-function cleanTime(tick){
+function leita() {
+      let input, filter, parent, child, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    parent = document.getElementById("hold");
+    child = parent.getElementsByTagName("div");
+    for (i = 0; i < child.length; i++) {
+        h2 = child[i].getElementsByTagName("h2")[0];
+        if (h2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            child[i].style.display = "";
+        } else {
+            child[i].style.display = "none";
 
+        }
+    }
+}
+
+function cleanTime(tick){
 
 };
